19d0d2aa3608c7fdfea03b0a7e30ed95f1880d0f
Ararab <70100282+KDY05@users.noreply.github.com>
2025-05-27 17:47:10 +0900

    Initial commit

---
+++ src/main/java/io/github/kdy05/sightLock/SightLock.java
@@ -0,0 +1,17 @@
+package io.github.kdy05.sightLock;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SightLock extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
